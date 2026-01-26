bcf6312f1e249965f9dbc903577c01f829428ec1
izumi.haruno <izumi.haruno@hamee.co.jp>
2026-01-27 01:22:15 +0900

    3-7完了

--- ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeQuadrantTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ComposeQuadrantApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,95 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ComposeQuadrantApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            ComposeQuadrantCard(
+                title = stringResource(R.string.first_quadrant_title),
+                description = stringResource(R.string.first_quadrant_description),
+                bgColor = colorResource(R.color.first_quadrant_bg_color),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+            ComposeQuadrantCard(
+                title = stringResource(R.string.second_quadrant_title),
+                description = stringResource(R.string.second_quadrant_description),
+                bgColor = colorResource(R.color.second_quadrant_bg_color),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+        }
+        Row(
+            modifier = Modifier.weight(1f)
+        ) {
+            ComposeQuadrantCard(
+                title = stringResource(R.string.third_quadrant_title),
+                description = stringResource(R.string.third_quadrant_description),
+                bgColor = colorResource(R.color.third_quadrant_bg_color),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+            ComposeQuadrantCard(
+                title = stringResource(R.string.fourth_quadrant_title),
+                description = stringResource(R.string.fourth_quadrant_description),
+                bgColor = colorResource(R.color.fourth_quadrant_bg_color),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun ComposeQuadrantCard(
+    title: String,
+    description: String,
+    bgColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
         modifier = modifier
-    )
+            .background(bgColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeQuadrantAppPreview() {
     ComposeQuadrantTheme {
-        Greeting("Android")
+        ComposeQuadrantApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeQuadrantCardPreview() {
+    ComposeQuadrantTheme {
+        ComposeQuadrantCard(
+            title = "Title",
+            description = "Description",
+            bgColor = Color.Green
+        )
     }
 }
